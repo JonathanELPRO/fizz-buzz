f93d48e3fbdf55527809b0cbf48ddf93dcc5e5c5
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-22 10:00:37 -0400

    Casos de prueba hasta el punto 2.2

---
+++ src/fizzbuzz.js
@@ -0,0 +1,14 @@
+
+function generarFizzbuzz(n){
+    // const FIZZ=3;
+    // if (n===FIZZ)
+    // {
+    //     return "fizz"
+    // }
+    if (n%3===0)
+    {
+        return "Fizz"
+    }
+    return n+"";
+}
+export default generarFizzbuzz
---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,16 @@
+import generarFizzbuzz from "./fizzbuzz.js";
+
+describe("FizzBuzz", () => {
+  it("Deberia generar el mismo numero para uno que no tiene regla para el 1", () => {
+    expect(generarFizzbuzz(1)).toEqual("1");
+  });
+  it("Deberia generar el mismo numero para uno que no tiene regla para el 2", () => {
+    expect(generarFizzbuzz(2)).toEqual("2");
+  });
+  it("Deberia generar el mismo numero para uno que no tiene regla para el 2", () => {
+    expect(generarFizzbuzz(3)).toEqual("Fizz");
+  });
+  it("Deberia generar Fizz para un numero que sigue una regla comoo los multiplos de 3", () => {
+    expect(generarFizzbuzz(6)).toEqual("Fizz");
+  });
+});
